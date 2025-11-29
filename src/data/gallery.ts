bae9b44e5faf1aa365c57a1d57a9ffc2d70d568c
dnhasly-alt <dnhasly@gmail.com>
2025-11-29 08:31:32 -0500

    modelaje

---
+++ src/data/gallery.ts
@@ -0,0 +1,22 @@
+export const galleryData = {
+  pasarela: [
+    "https://imgur.com/OR4DzKw.jpg",
+    "https://imgur.com/MRo4pXd.jpg",
+    "https://imgur.com/WpqQsa4.jpg",
+    "https://imgur.com/kvFgTB1.jpg",
+  ],
+
+  fotografia: [
+    "https://imgur.com/JcM4dbo.jpg",
+    "https://imgur.com/5VWgpPW.jpg",
+    "https://imgur.com/zW1Ziyj.jpg",
+    "https://imgur.com/gwWGmrA.jpg",
+  ],
+
+  modelaje: [
+    "https://imgur.com/zQfMQRV.jpg",
+    "https://imgur.com/n1l7lmR.jpg",
+    "https://imgur.com/z2e4iUX.jpg",
+    "https://imgur.com/RIFQkes.jpg",
+  ]
+};
